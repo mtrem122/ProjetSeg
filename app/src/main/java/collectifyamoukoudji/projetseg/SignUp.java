ceeb548222ab3fa26b0a642d27bab03812f4de5a
Erwin <erwinnzia@gmail.com>
2018-10-06 15:47:30 -0400

    la page sign up

--- app/src/main/java/collectifyamoukoudji/projetseg/SignUp.java
+++ app/src/main/java/collectifyamoukoudji/projetseg/SignUp.java
@@ -1,12 +1,17 @@
 package collectifyamoukoudji.projetseg;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_up);
     }
+
 }
